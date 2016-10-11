9aa92fe85bc12873e3450fe602454f5c5837af7d
justin leggett <justin@codingrookie.com>
2016-10-10 19:17:37 -0500

    completed the assigment

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,27 +1,52 @@
 "use strict";
 
-$(document).ready(function() {
+$(document).ready(() => {
   console.log("jquery is ready");
   var contentEl = $("#all-my-songs");
   var songs = [];
-  $.ajax({
-    url: "../songs.json"
-  }).done(function(data) {
-    songs = data.songs;
-    console.log("data", data);
+
+function getSongs() {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "../songs.json"
+    }).done((data) => {
+      resolve(data);
+    }).fail((xhr, status, error) => {
+      reject(error);
+    });
+  });
+}
+
+
+function getSongs2(resultOfFirstAjax) {
+  return new Promise((resolve, reject) => {
     $.ajax({
       url: "../songs2.json"
-      }).done(function(data2) {
-        console.log("data2", data2);
+    }).done((data2) => {
+      songs = resultOfFirstAjax.songs;
+      resolve(data2);
+    }).fail((xhr2, status2, error2) => {
+      reject(error2);
+    });
+  });
+}
+
+getSongs().then((dataPass)  => {
+  console.log("dataPass", dataPass);
+  console.log("song 1st then", songs);
+  return getSongs2(dataPass);
+}).then(function(dataPass2) {
+  console.log("songs 2nd then", songs);
+  console.log("dataPass2", dataPass2);
         var songData = "";
         var currentSong;
-        
-        data2.songs.forEach(function(song){
+
+        dataPass2.songs.forEach(function(song){
           songs.push(song);
         });
 
         console.log("songs", songs);
-        
+
         for (var i = 0; i < songs.length; i++) {
           currentSong = songs[i];
 
@@ -38,10 +63,8 @@ $(document).ready(function() {
 
         console.log("songData", songData);
         contentEl.html(songData);
-      }).fail(function(error) {
-        console.log( "error" , error);
-      });
-  }).fail(function(error) {
-    console.log( "error" , error);
   });
+
 });
+
+
